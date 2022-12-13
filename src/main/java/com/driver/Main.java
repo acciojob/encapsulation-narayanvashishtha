91635284c67c3dcf9f7f2fdfc1da8993fa9d0ca0
Narayan <vashisthnarayan98@gmail.com>
2022-12-13 18:32:35 +0530

    3rd Question, 1st attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //obj.name = 30;
+        //'name' has private access in 'com.diver.RWOnly'
+        obj.setName(69);
+        obj.getName();
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,20 @@
 package com.driver;
 
+
 public class RWOnly {
+
+    private int name;
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+
+    }
     
 }
